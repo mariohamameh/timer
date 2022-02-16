ee019660e1dc0fb64982f250f6240b3ed215a80a
mariohamameh <mariohamameh97@gmail.com>
2022-02-16 01:01:24 +0000

    added timer

---
+++ timer1.js
@@ -0,0 +1,16 @@
+const scheduleBeep = function(delay) {   
+    delay = Number(delay);   
+    if (!isNaN(delay) && delay > 0) {     
+    setTimeout(beep, delay * 1000);   
+    } 
+}
+
+const beep = function() {
+
+    process.stdout.write("BEEP!");
+
+} 
+
+const numbers = process.argv.slice(2);  
+for (const num of numbers) {   scheduleBeep(num); }
+
